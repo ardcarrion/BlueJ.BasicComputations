e87a58146d3dda0f32110458030c9878d1dad5da
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-23 13:42:20 -0400

    Working without class method calls

--- IntegerPrinter.java
+++ IntegerPrinter.java
@@ -1,21 +1,34 @@
  
 
+import java.util.Scanner;
 
 public class IntegerPrinter {
 
     public String printIntegerAsBinary(int value){
-        return null;
+        return Integer.toBinaryString(value);
     }
 
     public String printIntegerAsOctal(int value){
-        return null;
+        return Integer.toOctalString(value);
     }
 
     public String printIntegerAsHexadecimal(int value){
-        return null;
+        return Integer.toHexString(value);
     }
 
     public static void main(String[] args){
-
+        System.out.println("Give me an integer:");
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        
+        String binaryRep = Integer.toBinaryString(number);
+        String octalRep = Integer.toOctalString(number);
+        String hexRep = Integer.toHexString(number);
+        
+        System.out.printf("In binary %d is: %s\n", number, binaryRep);
+        System.out.printf("In octal %d is: %s\n", number, octalRep);
+        System.out.printf("In hexadecimal %d is: %s\n", number, hexRep);
+        
+      
     }
 }
